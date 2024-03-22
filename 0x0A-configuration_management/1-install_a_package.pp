c8fd9471e10beeb76f308884153999cd1b049460
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-03-22 18:25:31 +0100

    Add Flask and Werkzeug packages using pip3 provider

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,3 +3,9 @@ package { 'Flask':
         ensure   => '2.1.0',
         provider => 'pip3',
 }
+
+# Install Werkzeug version 2.1.1 using pip3 provider
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
