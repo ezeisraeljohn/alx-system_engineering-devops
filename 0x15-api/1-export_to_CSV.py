538dbf8ef58ad6eeba19a6d3b27426f56a5c51b1
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-04-23 12:30:25 +0100

    Refactor CSV export script in 0x15-api/1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3
 
+
 """ This module talks about how this will be implemented """
 
-from sys import argv
-import requests
+
+gett = __import__("0-gather_data_from_an_API")
 
 
-userId = argv[1]
 if __name__ == "__main__":
-    r_todos = requests.get("https://jsonplaceholder.typicode.com/todos/")
-    r_employee = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                              .format(userId))
-    tasks = [task for task in r_todos.json()
-             if int(task.get("userId")) == int(userId)]
 
-    NAME = r_employee.json().get('name')
 
-    with open(f"{userId}.csv", "w") as file:
-        for task in tasks:
-            file.write('"{}","{}","{}","{}"\n'.format(task.get("userId"),
-                                                      NAME,
+    with open(f"{gett.id}.csv", "w") as file:
+        for task in gett.tasks:
+            file.write('"{}","{}","{}","{}"\n'.format(gett.id, gett.NAME,
                                                       task.get("completed"),
                                                       task.get("title")))
