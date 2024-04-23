d02745716cf6ab036366852a71c6973f2274ebf2
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-04-23 02:03:49 +0100

    Add script to export API data to CSV in 0x15-api/1-export_to_CSV.py

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+""" This scipt stores data in a csv file """
+
+
+from sys import argv
+import requests
+
+
+userId = argv[1]
+if __name__ == "__main__":
+    r_todos = requests.get("https://jsonplaceholder.typicode.com/todos/")
+    r_employee = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                              .format(userId))
+    tasks = [task for task in r_todos.json()
+             if int(task.get("userId")) == int(userId)]
+
+    NAME = r_employee.json().get('name')
+
+    with open(f"{userId}.csv", "w") as file:
+        for task in tasks:
+            file.write('"{}","{}","{}","{}"\n'.format(task.get("userId"),
+                                                      NAME,
+                                                      task.get("completed"),
+                                                      task.get("title")))
