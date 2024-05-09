38031e11567e167348ddd3b9486d1eafd723d391
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-05-09 22:43:56 +0100

    feat: Add function to retrieve top 10 posts from a subreddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+import requests
+from pprint import pprint
+from sys import argv
+
+
+def top_ten(subreddit):
+    """ Function returns the top 10 posts on a given subreddit """
+
+    url = f'http://reddit.com/r/{subreddit}/top.json'
+    headers = {'user-agent': 'MyScript/1.0'}
+#     params = {'limit': 20}
+
+    response = requests.get(url, headers=headers)
+    data = response.json().get('data', {}).get('children', [])
+
+    posts = []
+    for post in data:
+        post_data = post.get('data', {})
+        post_info = {
+            'title': post_data.get('title', ''),
+            'url': post_data.get('url', ''),
+            'num_comments': post_data.get('num_comments', 0)
+        }
+        posts.append(post_info)
+
+        return posts
+
+
+print(top_ten(argv[1]))
