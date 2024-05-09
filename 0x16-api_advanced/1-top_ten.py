7cb1d753c54ab72c7cd5e5a719c1f46cb2a397b4
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-05-09 23:34:33 +0100

    feat: Refactor code to include 'allow_redirects' parameter in 0-subs.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,30 +1,24 @@
 #!/usr/bin/python3
 
+"""Returns top ten posts."""
 
-""" This script will retrieve top ten post of a subreddit"""
 import requests
-from pprint import pprint
-from sys import argv
 
 
 def top_ten(subreddit):
-    """ Function returns the top 10 posts on a given subreddit """
+    """Returns the top ten posts"""
+    # Send a GET request to the Reddit API to retrieve the top 10 hot posts
+    response = requests.get(
+        "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit),
+        headers={"User-Agent": "nabuntu_bot-01"},
+        allow_redirects=False,
+    )
 
-    url = f'http://reddit.com/r/{subreddit}/top.json'
-    headers = {'user-agent': 'MyScript/1.0'}
-#     params = {'limit': 20}
+    # Check if the request was successful
+    if response.status_code != 200:
+        print(None)
+        return
 
-    response = requests.get(url, headers=headers)
-    data = response.json().get('data', {}).get('children', [])
-
-    posts = []
-    for post in data:
-        post_data = post.get('data', {})
-        post_info = {
-            'title': post_data.get('title', ''),
-            'url': post_data.get('url', ''),
-            'num_comments': post_data.get('num_comments', 0)
-        }
-        posts.append(post_info)
-
-        return posts
+    # Print the titles of the top 10 hot posts
+    for post in response.json()["data"]["children"]:
+        print(post["data"]["title"])
